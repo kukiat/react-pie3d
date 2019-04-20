b1417fbb5984cd4062de0f7999f0cf577cf19efd
kukiat <kukiat1996@gmail.com>
2019-04-21 02:51:53 +0700

    Remove propsType

--- src/slice/label.js
+++ src/slice/label.js
@@ -4,18 +4,6 @@ import move from '.././math/move';
 import midlleAngle from '.././math/middleAngle';
 
 export default class Label extends React.Component {
-
-  static propTypes = {
-    d: React.PropTypes.shape({
-      label: React.PropTypes.string,
-      endAngle: React.PropTypes.number,
-      startAngle: React.PropTypes.number
-    }).isRequired,
-    rx: React.PropTypes.number.isRequired,
-    ry: React.PropTypes.number.isRequired,
-    h: React.PropTypes.number.isRequired,
-  }
-
   render() {
     const middle = midlleAngle(this.props.d.startAngle, this.props.d.endAngle);
     const translate = move(this.props.d, this.props.rx, this.props.ry);
@@ -26,11 +14,11 @@ export default class Label extends React.Component {
     ];
     return (
       <text fontSize={10}
-            transform={this.props.moved ?
-              `translate(${[position[0] + translate[0], position[1] + translate[1]]})`
-              :
-              `translate(${position})`}
-            textAnchor={middle > 3 / 2 * Math.PI || middle < Math.PI / 2 ? 'start' : 'end'}
+        transform={this.props.moved ?
+          `translate(${[position[0] + translate[0], position[1] + translate[1]]})`
+          :
+          `translate(${position})`}
+        textAnchor={middle > 3 / 2 * Math.PI || middle < Math.PI / 2 ? 'start' : 'end'}
       >
         {this.props.d.label ? `${this.props.d.label}(${
           ((this.props.d.endAngle - this.props.d.startAngle) / (2 * Math.PI) * 100).toFixed(0)}%)` :
